4f9273a7c4f1e80cf8ed963ff899335f71b9f199
Ted Xie <tedxie01@gmail.com>
2021-04-11 17:16:03 -0700

    Augment DSLX Makefile flow

--- common.mk
+++ common.mk
@@ -2,21 +2,37 @@ XLS_HOME ?= ~/sw/xls
 XLSCC=${XLS_HOME}/bazel-bin/xls/contrib/xlscc/xlscc
 XLS_OPT_MAIN=${XLS_HOME}/bazel-bin/xls/tools/opt_main
 XLS_CODEGEN_MAIN=${XLS_HOME}/bazel-bin/xls/tools/codegen_main
+XLS_DSLX_INTERP=$(XLS_HOME)/bazel-bin/xls/dslx/interpreter_main
+XLS_DSLX_IR_CONV=${XLS_HOME}/bazel-bin/xls/dslx/ir_converter_main
 CLANG_HOME=/usr/include/clang/10.0.0/include
 
+ifneq (${DSLX},1)
 ${NAME}: ${SRCS} ${HDRS}
 	${CXX} ${CPPFLAGS} ${CXXFLAGS} ${SRCS} -o ${NAME}
+else
+${NAME}: ${SRCS} ${HDRS}
+	${XLS_DSLX_INTERP} --compare_jit=false ${SRCS}
+endif
 
+ifneq (${DSLX},1)
 run: ${NAME}
 	./${NAME} ${EXEC_ARGS}
+else
+run: ${NAME}
+endif
 
 clang_args:
 	echo '-D__SYNTHESIS__ \
 -I${XLS_HOME} \
 -I${CLANG_HOME}' > clang.args
 
-${NAME}.ir: ${SRCS} ${HDRS} clang_args ${NAME}
+ifneq (${DSLX},1)
+${NAME}.ir: clang_args ${NAME}
 	${XLSCC} --clang_args_file clang.args ${SRCS} > ${NAME}.ir
+else
+${NAME}.ir: ${NAME}
+	${XLS_DSLX_IR_CONV} ${SRCS} > ${NAME}.ir
+endif
 
 ${NAME}_opt.ir: ${NAME}.ir
 	${XLS_OPT_MAIN} ${NAME}.ir > ${NAME}_opt.ir
@@ -29,4 +45,4 @@ ${NAME}_opt.v: ${NAME}_opt.ir
 
 
 clean:
-	rm -f *.o ${NAME} clang.args *.ir
+	rm -f *.o ${NAME} clang.args *.ir *.v
